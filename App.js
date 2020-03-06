dd79ec270aecc8a1865f17f3aa6d2df849ae2dd6
David <41311088+biagiola@users.noreply.github.com>
2020-03-06 10:56:28 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,27 +1,43 @@
+  
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import Header from './components/header';
 import TodoItem from './components/todoItem';
+import AddTodo from './components/addTodo';
 
 export default function App() {
   const [todos, setTodos] = useState([
     { text: 'buy coffee', key: '1' },
     { text: 'create an app', key: '2' },
-    { text: 'play on the switch', key: '3' }
+    { text: 'play on the switch', key: '3' },
   ]);
 
   const pressHandler = (key) => {
-      setTodos( prevTodos => {
-          return prevTodos.filter(todo => todo.key != key);
-      });
+    setTodos(prevTodos => {
+      return prevTodos.filter(todo => todo.key != key);
+    });
   };
 
+  const submitHandler = (text) => {
+    if(text.length > 3){
+      setTodos(prevTodos => {
+        return [
+          { text, key: Math.random().toString() },
+          ...prevTodos
+        ];
+      });
+    } else {
+      Alert.alert('OOPS', 'Todo must be over 3 characters long', [
+        {text: 'Understood', onPress: () => console.log('alert closed') }
+      ]);
+    }
+  };
 
   return (
     <View style={styles.container}>
       <Header />
       <View style={styles.content}>
-        {/* add todo form */}
+        <AddTodo submitHandler={submitHandler} />
         <View style={styles.list}>
           <FlatList
             data={todos}
