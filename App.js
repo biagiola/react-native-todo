bf02c5bfc94cbe61a868c491a0941f009394aa07
David <41311088+biagiola@users.noreply.github.com>
2020-03-06 08:37:31 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,28 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Header from './components/header';
 
 export default function App() {
-
-  const [todos, settodos] = useState([
-      { test:'buy coffee' , key: '1'},
-      { test:'create an app' , key: '2'},
-      { test:'play on the switch' , key: '3'}
+  const [todos, setTodos] = useState([
+    { text: 'buy coffee', key: '1' },
+    { text: 'create an app', key: '2' },
+    { text: 'play on the switch', key: '3' }
   ]);
+
 
   return (
     <View style={styles.container}>
-        <header/>
-        <View style={styles.content}>
-            {/* add todo form */}
-            <View style={styles.list}>
-                <FlatList
-                    data={todos}
-                    renderItem={ ({item}) => (
-                        <Text>{item.text}</Text>
-                    )}
-                />
-            </View>
+      <Header />
+      <View style={styles.content}>
+        {/* add todo form */}
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => (
+              <Text>{item.text}</Text>
+            )}
+          />
         </View>
+      </View>
     </View>
   );
 }
@@ -33,9 +34,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
   content: {
-      padding: 40,
+    padding: 40,
   },
   list: {
-      marginTop: 20,
+    marginTop: 20,
   },
 });
